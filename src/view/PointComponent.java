f9aea173ad4deba2dd20306957e08dbfdf2114d6
Mateusz Słomka <slomkam7@gmail.com>
2024-06-07 16:39:22 +0200

    add displaying static powerups

--- src/view/PointComponent.java
+++ src/view/PointComponent.java
@@ -4,7 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PointComponent extends JPanel {
-    public PointComponent() {
+    private final int size;
+    private final Color color;
+    private boolean isVisible;
+    public PointComponent(int size, Color color) {
+        this.size = size;
+        this.color = color;
         setBackground(new Color(0, 0, 0, 0));
     }
 
@@ -12,6 +17,16 @@ public class PointComponent extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         var s = getSize();
-        g.fillOval(s.width / 2 - 5, s.height / 2 - 5, 10, 10);
+        g.setColor(color);
+        g.fillOval((s.width - size) / 2, (s.height - size) / 2, size, size);
+    }
+
+    @Override
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean isVisible) {
+        this.isVisible = isVisible;
     }
 }
--- src/view/SwingedField.java
+++ src/view/SwingedField.java
@@ -8,6 +8,7 @@ import java.awt.*;
 public class SwingedField extends JPanel implements ComponentUpdatable {
     private final Field field;
     private final PointComponent point;
+    private final PointComponent staticPowerup;
 
     public SwingedField(Field f) {
         field = f;
@@ -16,16 +17,34 @@ public class SwingedField extends JPanel implements ComponentUpdatable {
         setBackground(color);
         setLayout(new BorderLayout());
 
-        point = new PointComponent();
+        point = new PointComponent(10, Color.LIGHT_GRAY);
+        staticPowerup = new PointComponent(20, Color.LIGHT_GRAY);
+        add(staticPowerup);
         add(point);
         update();
     }
 
-    public void update() {
-        var s = getSize();
-        if (point.isVisible() != field.hasPoint()) {
-            point.setLocation(s.width / 2, s.height / 2);
-            point.setVisible(field.hasPoint());
+    private void set(PointComponent p, boolean isVisible) {
+        p.setVisible(isVisible);
+        if (isVisible) {
+            removeAll();
+            add(p);
+        } else {
+            remove(p);
         }
+    }
+
+    public void update() {
+        if (staticPowerup.isVisible() != field.hasPowerup()) {
+            set(staticPowerup, field.hasPowerup());
+            repaint();
+            return;
+        }
+
+        if (point.isVisible() != field.hasPoint()) {
+            set(point, field.hasPoint());
+            repaint();
+        }
+
     }
 }
