6872e6245a69eb7164589e04b04ed32fe4d562bb
Mateusz Słomka <slomkam7@gmail.com>
2024-06-09 10:28:39 +0200

    add new board

--- src/view/SwingedStateDisplay.java
+++ src/view/SwingedStateDisplay.java
@@ -1,34 +1,53 @@
 package view;
 
 import game.PacmanBoard;
+import utils.ClockDisplay;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class SwingedStateDisplay extends JPanel implements ComponentUpdatable {
-    private JLabel pointsLabel;
+    private final JLabel pointsLabel;
+    private final JLabel timeLabel;
+    private final JLabel levelLabel;
     private final PacmanBoard board;
     private int prevScore = -1;
     public SwingedStateDisplay(PacmanBoard board) {
         this.board = board;
-        setLayout(new FlowLayout(FlowLayout.LEFT));
+        var layout = new FlowLayout(FlowLayout.LEFT);
+        layout.setHgap(20);
+        setLayout(layout);
 
-        pointsLabel = new JLabel();
-        pointsLabel.setForeground(Color.WHITE);
-        pointsLabel.setFont(new Font(null, Font.BOLD, 30));
-        pointsLabel.setBackground(new Color(0, 0, 0, 0));
+
+        pointsLabel = createLabel();
         add(pointsLabel);
 
-        setBounds(0, 0, 400, 100);
-        setBackground(new Color(0, 0, 0, 0));
+        timeLabel = createLabel();
+        add(timeLabel);
+
+        levelLabel = createLabel();
+        add(levelLabel);
+
+        setBounds(0, 0, 450, 50);
+        setBackground(Color.WHITE);
+    }
+
+    private JLabel createLabel() {
+        var defaultFont = new Font(null, Font.BOLD, 30);
+        var label = new JLabel();
+        label.setFont(defaultFont);
+        return label;
     }
 
     @Override
     public void update(double fieldSize) {
         int score = board.getPlayer().getScore();
+        var display = new ClockDisplay(board.getClock());
         if (prevScore != score) {
             pointsLabel.setText("Score: " + score);
             prevScore = score;
         }
+        timeLabel.setText(display.getFormattedTime());
+        levelLabel.setText("Level: " + board.getLevel());
     }
 }
